dfcc5d37c931e013b92c4d07ea05be2a00d0cdd5
Simon <tobertsi@hs-pforzheim.de>
2019-06-06 13:40:01 +0200

    Löschen von unsicheren Files

--- wikibase-sdk-master/test/lib/tests_env.js
+++
@@ -1,8 +0,0 @@
-const instance = 'https://www.wikidata.org'
-const sparqlEndpoint = 'https://query.wikidata.org/sparql'
-
-module.exports = {
-  instance,
-  sparqlEndpoint,
-  buildUrl: require('../../lib/utils/build_url')(instance)
-}
