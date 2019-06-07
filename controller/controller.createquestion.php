5c05364cb89336d9ca97caddebb5515b1197d381
Simon <tobertsi@hs-pforzheim.de>
2019-06-07 20:05:19 +0200

    Die ersten Skripte zum generieren von Fragen KAT: Geographie

--- controller/controller.createquestion.php
+++ controller/controller.createquestion.php
@@ -6,7 +6,7 @@
  * and open the template in the editor.
  */
 
-$url = "https://query.wikidata.org/sparql?format=json&query=SELECT%20DISTINCT%20%3Fcity%20%3FcityLabel%20%3Fpopulation%20%3Fcountry%20%3FcountryLabel%20%3Floc%20WHERE%20%7B%0A%09%7B%0A%09%09SELECT%20(MAX(%3Fpopulation)%20AS%20%3Fpopulation)%20%3Fcountry%20WHERE%20%7B%0A%09%09%09%3Fcity%20wdt%3AP31%2Fwdt%3AP279*%20wd%3AQ515%20.%0A%09%09%09%3Fcity%20wdt%3AP1082%20%3Fpopulation%20.%0A%09%09%09%3Fcity%20wdt%3AP17%20%3Fcountry%20.%0A%09%09%7D%0A%09%09GROUP%20BY%20%3Fcountry%0A%09%09ORDER%20BY%20DESC(%3Fpopulation)%0A%09%7D%0A%09%3Fcity%20wdt%3AP31%2Fwdt%3AP279*%20wd%3AQ515%20.%0A%09%3Fcity%20wdt%3AP1082%20%3Fpopulation%20.%0A%09%3Fcity%20wdt%3AP17%20%3Fcountry%20.%0A%09%3Fcity%20wdt%3AP625%20%3Floc%20.%0A%09SERVICE%20wikibase%3Alabel%20%7B%0A%09%09bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20.%0A%09%7D%0A%7D%0AORDER%20BY%20DESC%20(%3Fpopulation)";
+$url = "https://query.wikidata.org/sparql?format=json&query=SELECT%20DISTINCT%20%3Fcountry%20%3FcountryLabel%20%3Fcapital%20%3FcapitalLabel%20%3Fcountrypopulation%20%3Flanguage%20%3FlanguageLabel%20%3Fcurrency%20%24currencyLabel%20%3Fhighestpoint%20%3FhighestpointLabel%20%3Flowestpoint%20%3FlowestpointLabel%20%3Fheadofgoverment%20%3FheadofgovermentLabel%20%3Fformofgoverment%20%3FformofgovermentLabel%20%3FHDI%20%3Fscoollesschild%20%3Funemployment%0AWHERE%0A%7B%0A%20%20%3Fcountry%20wdt%3AP31%20wd%3AQ3624078%20.%0A%20%20%23not%20a%20former%20country%0A%20%20FILTER%20NOT%20EXISTS%20%7B%3Fcountry%20wdt%3AP31%20wd%3AQ3024240%7D%0A%20%20%23and%20no%20an%20ancient%20civilisation%20(needed%20to%20exclude%20ancient%20Egypt)%0A%20%20FILTER%20NOT%20EXISTS%20%7B%3Fcountry%20wdt%3AP31%20wd%3AQ28171280%7D%0A%3Fcountry%20wdt%3AP36%20%3Fcapital%3B%0Awdt%3AP1082%20%3Fcountrypopulation%20%3B%0A%23wdt%3AP37%20%3Flanguage%20%3B%0Awdt%3AP38%20%3Fcurrency%3B%0Awdt%3AP610%20%3Fhighestpoint%3B%0Awdt%3AP1589%20%3Flowestpoint%3B%0Awdt%3AP6%20%3Fheadofgoverment%3B%0A%23wdt%3AP122%20%3Fformofgoverment%3B%0A%23wdt%3AP1081%20%3FHDI%3B%0Awdt%3AP2573%20%3Fscoollesschild%3B%20%20%20%20%20%20%20%20%20%0A%23wdt%3AP1198%20%3Funemployment.%0A%20%20%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22de%22%7D%0A%7D%0A%20%0AORDER%20BY%20%3FcountryLabel%0ALIMIT%20500";
 
 
 $string = file_get_contents($url);
@@ -17,60 +17,159 @@ $json_result = json_decode($string, true);
 $results = $json_result['results'];
 $bindingsarray = $results['bindings'];
 
-for ($i = 0; $i < 1; $i++) {
+for ($i = 1; $i <= count($bindingsarray); $i++) {
     
+$bindings = $results['bindings'][$i] ; 
+$countrylabel = $bindings['countryLabel'];
+$countryname = $countrylabel['value'];
+
+$bindings = $results['bindings'][$i] ; 
+$capitallabel = $bindings['capitalLabel'];
+$capitalname = $capitallabel['value'];
+
+$bindings = $results['bindings'][$i] ; 
+$countrypopulation = $bindings['countrypopulation'];
+$pop = $countrypopulation['value'];
+
 $bindings = $results['bindings'][$i] ; 
 $citylabel = $bindings['cityLabel'];
 $cityname = $citylabel['value'];
 
+$bindings = $results['bindings'][$i] ; 
+$currencylabel = $bindings['currencyLabel'];
+$currencyname = $currencylabel['value'];
 
+$bindings = $results['bindings'][$i] ; 
+$highestpointLabel = $bindings['highestpointLabel'];
+$highestpointname = $highestpointLabel['value'];
 
+$bindings = $results['bindings'][$i] ; 
+$lowestpointLabel = $bindings['lowestpointLabel'];
+$lowestpointname = $lowestpointLabel['value'];
 
+$bindings = $results['bindings'][$i] ; 
+$headofgovermentLabel = $bindings['headofgovermentLabel'];
+$headofgovermentname = $headofgovermentLabel['value'];
 
-$population = $bindings['population'];
-$Menge = $population['value'];
+$bindings = $results['bindings'][$i] ; 
+$scoollesschild = $bindings['scoollesschild'];
+$childs = $scoollesschild['value'];
 
-$countrylable = $bindings['countryLabel'];
-$country = $countrylable['value'];
-echo ($country);
-echo ($Menge); 
-echo ($cityname);  
+$meinefrage ="Was ist die Hauptstadt von $countryname?";
+$meinekat =8;
+$meineAntwort =$capitalname;
+$falscheAntwort ="";
+$falscheAntwortlabel ='capitalLabel';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
+
+$meinefrage ="Wer ist Staatoberhaupt von $countryname?";
+$meinekat =8;
+$meineAntwort =$headofgovermentname;
+$falscheAntwort ="";
+$falscheAntwortlabel ='headofgovermentLabel';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
+
+$meinefrage ="Wie viele Einwohner hat $countryname?";
+$meinekat =8;
+$meineAntwort =$pop;
+$falscheAntwort ="";
+$falscheAntwortlabel ='countrypopulation';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
+
+$meinefrage ="Welche Waehrung hat $countryname?";
+$meinekat =8;
+$meineAntwort =$currencyname;
+$falscheAntwort ="";
+$falscheAntwortlabel ='currencyLabel';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
+
+$meinefrage ="Wie heißt der höchste Punkt in $countryname? Tipp: Meistens ist das ein Berg ;)";
+$meinekat =8;
+$meineAntwort =$highestpointname;
+$falscheAntwort ="";
+$falscheAntwortlabel ='highestpointLabel';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
+
+$meinefrage ="Wie heißt der niedrigste Punkt in $countryname? Tipp: Meistens sind das Seen und Flüsse ;)";
+$meinekat =8;
+$meineAntwort =$lowestpointname;
+$falscheAntwort ="";
+$falscheAntwortlabel ='lowestpointLabel';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
+
+$meinefrage ="Bildung ist wichtig, aber nicht jedes Kind geht in die Schule! Wie viele Kinder gehen in $countryname nicht in die Schule?";
+$meinekat =8;
+$meineAntwort =$childs;
+$falscheAntwort ="";
+$falscheAntwortlabel ='scoollesschild';
+createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray);
 }
 
 
 
+
+function createmyquestion($meinefrage, $meinekat, $meineAntwort, $falscheAntwort,$falscheAntwortlabel,$results,$bindingsarray) {
+
 $frage=new Frage;
-
-$frage->beschreibung = "Was ist die Hauptstadt von $country ?";
-$frage->Kategorie = 8;
+$frage->beschreibung = $meinefrage;
+$frage->Kategorie = $meinekat;
 $frage->antwortzeit = 10;
-
-        
-
-
 $frage->create(); // gibt Im Idealfall eingegebene ID zurück, sonst 0/false
 
 
 $antwort=new Antwort;
-
-$antwort->Antworttext=$cityname;
+$antwort->Antworttext=$meineAntwort;
 $antwort->korrekt=1;
 $antwort->to_Frage = $frage->m_oid;  
 $antwort->create();
 
+do { 
+ $bindings = $results['bindings'][rand(0, count($bindingsarray))] ; 
+ $erstA = $bindings[$falscheAntwortlabel];
+$erstA=$erstA['value']; 
+ } 
+ while ($erstA ==$meineAntwort);
+ 
+ 
+ do {
+ $bindings = $results['bindings'][rand(0, count($bindingsarray))] ; 
+ $zweitA = $bindings[$falscheAntwortlabel];
+ $zweitA =$zweitA ['value'];
+  } 
+ while (($erstA ==$zweitA)AND ($zweitA==$meineAntwort));
+ 
+ do {
+ $bindings = $results['bindings'][rand(0, count($bindingsarray))] ; 
+ $drittA = $bindings[$falscheAntwortlabel];
+ $drittA=$drittA['value'];
+
+} while (($drittA ==$zweitA)AND ($drittA==$meineAntwort)AND($drittA ==$erstA));
+
+$falscheAntwort =[
+    0 => $erstA,
+     1 =>$zweitA,
+     2 =>$drittA];
 
 
-
-for ($i = 0; $i < 3; $i++) {
+for ($i = 0; $i < 3; $i++) { 
+ 
     
-$bindings = $results['bindings'][rand(0, count($bindingsarray))] ; 
-$citylabel = $bindings['cityLabel'];
-$wrongcityname = $citylabel['value']; 
     
-$antwort->Antworttext=$wrongcityname;
+  
+//do {    
+//     $bindings = $results['bindings'][rand(0, count($bindingsarray))] ; 
+//    $citylabel = $bindings[$falscheAntwortlabel];
+//    $wrongcityname = $citylabel['value'];    
+//    
+//} while ($meineAntwort == $wrongcityname);
+    
+   
+
+$antwort->Antworttext=$falscheAntwort[$i];
 $antwort->korrekt=0;
 $antwort->to_Frage = $frage->m_oid;  
 $antwort->create();
+} 
 
 };
 
