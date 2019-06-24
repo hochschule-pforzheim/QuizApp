0599accedb8ab039335405391ae770c26793a7d2
eisfeldt <51491929+eisfeldt@users.noreply.github.com>
2019-06-24 16:00:44 +0200

    Update main_navbar.php
    Fragen generieren Button nur für Admins sichtbar

--- includes/main_navbar.php
+++ includes/main_navbar.php
@@ -2,10 +2,10 @@
     <a href="?task=home" data-role="button" data-icon="home" data-theme="a" data-ajax="false" >Home</a>
     <?php if(Core::$user->gruppe==2){ ?>
     <a href="?task=fragenlist" data-role="button" data-icon="home" data-theme="a" data-ajax="false" >Fragen</a>
+    <a href="?task=fragenersteller" data-role="button" data-icon="upload" data-theme="b" data-ajax="false" >Fragen generieren</a>
     <?php } ?>
     <?php if(Core::$user->gruppe>=1){ ?>
     <a href="?task=spielerstellen" data-role="button" data-icon="plus" data-theme="a" data-ajax="false" >Spiel erstellen</a>
-         <a href="?task=fragenersteller" data-role="button" data-icon="upload" data-theme="b" data-ajax="false" >Fragen erstellen</a>
     <?php } ?>
       <?php if($_SESSION['uid']==""){?> <a href="?task=login" data-role="button" data-icon="user"  data-ajax="false">login</a><?php }?>
    <?php if($_SESSION['uid']!=""){?><a href="?task=logout" data-role="button" data-icon="sign-out"  data-ajax="false">logout</a><?php }?>
