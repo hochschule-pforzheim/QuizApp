79c05a519203940e17589432fd574ae3864a9485
eisfeldt <51491929+eisfeldt@users.noreply.github.com>
2019-06-25 17:44:24 +0200

    Update main_navbar.php

--- includes/main_navbar.php
+++ includes/main_navbar.php
@@ -2,7 +2,7 @@
     <a href="?task=home" data-role="button" data-icon="home" data-theme="a" data-ajax="false" >Home</a>
     <?php if(Core::$user->gruppe==2){ ?>
     <a href="?task=fragenlist" data-role="button" data-icon="home" data-theme="a" data-ajax="false" >Fragen</a>
-    <a href="?task=fragenersteller" data-role="button" data-icon="upload" data-theme="b" data-ajax="false" >Fragen generieren</a>
+    <a href="?task=fragenersteller" data-role="button" data-icon="upload" data-theme="a" data-ajax="false" >Fragen generieren</a>
     <?php } ?>
     <?php if(Core::$user->gruppe>=1){ ?>
     <a href="?task=spielerstellen" data-role="button" data-icon="plus" data-theme="a" data-ajax="false" >Spiel erstellen</a>
